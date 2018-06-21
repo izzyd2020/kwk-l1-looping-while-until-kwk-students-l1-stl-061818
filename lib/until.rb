c40d25594ea6da8a7f002694ed093fc660268029
Isabella Duran <2020ikd7@gmail.com>
2018-06-21 16:56:31 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,15 @@
 def using_until
         levitation_force = 6
         #your code here
-    
+    loop do 
+      if levitation_force == 10 
+        break 
+      end 
+      puts "more force"
+      sleep(0.5)
+      levitation_force = levitation_force + 1 
+    puts "you have a levitation force of #{levitation_force}"
+    end 
 end
+using_until
 
